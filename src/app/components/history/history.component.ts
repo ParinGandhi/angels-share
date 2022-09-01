9a62a7a1014c858962598b0449e140d3dead157a
kookiedoodle5 <parin.a.gandhi@gmail.com>
2022-08-31 22:46:04 -0400

    Updated history

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -9,13 +8,8 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class HistoryComponent implements OnInit {
   modalRef?: BsModalRef;
-  testJson =
-    '<p>The first written record of distilling is found. Ancient Greek philosopher<a href="javascript:void(0)" (click)="openModal(alexanderOfAphrodisias)">Alexander of Aphrodisias</a>describes the process of taking sea water and distilling it into pure drinking water. Medieval civilizations evolved their techniques over the following centuries, although still not resulting in alcohol.</p>';
-  safeHtml: SafeHtml;
 
-  constructor(private modalService: BsModalService, public dom: DomSanitizer) {
-    this.safeHtml = dom.bypassSecurityTrustHtml(this.testJson);
-  }
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit(): void {}
 
